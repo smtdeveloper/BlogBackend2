2b8e5c1fe6404bcad025c177d14aa9cbe84994fa
Samet Akca <+akcasamet99@users.noreply.github.com>
2021-08-13 01:51:57 +0300

    Category icin update ve getId yazıldı ve test edildi calısıyor

--- BlogBackend2/Controllers/CategoriesController.cs
+++ BlogBackend2/Controllers/CategoriesController.cs
@@ -1,5 +1,6 @@
 ﻿using BlogBackend2.EntityFramework.DbContexts;
 using BlogBackend2.Models.Dtos;
+using BlogBackend2.Models.Dtos.Category;
 using BlogBackend2.Models.Entities;
 using BlogBackend2.Utilities.Constants;
 using BlogBackend2.Utilities.Results;
@@ -14,11 +15,11 @@ namespace BlogBackend2.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
-        public PostgreDbContext dbContext;
+        public PostgreDbContext _postgreDbContext;
 
         public CategoriesController(PostgreDbContext dbContext)
         {
-            this.dbContext = dbContext;
+            this._postgreDbContext = dbContext;
         }
 
         [HttpPost("add")]
@@ -31,21 +32,43 @@ namespace BlogBackend2.Controllers
                 IsDeleted = false
             };
 
-            if (dbContext.Categories.Any(p => p.Name.Equals(addDto.Name)))
+            if (_postgreDbContext.Categories.Any(p => p.Name.Equals(addDto.Name)))
                 return BadRequest(new Result(success: false,Messages.CategoryNotFound));
 
-            dbContext.Add(category);
-            bool addResult = dbContext.SaveChanges() > 0;
+            _postgreDbContext.Add(category);
+            bool addResult = _postgreDbContext.SaveChanges() > 0;
             if (addResult)
-                return Ok(new Result(success: true, message: Messages.CategoryNotAdded));
+                return Ok(new Result(success: true, message: Messages.CategoryAdded));
 
-            return BadRequest(new Result(success: false, message: Messages.CategoryAdded));
+            return BadRequest(new Result(success: false, message: Messages.CategoryNotAdded));
         }
+
+        [HttpPut("update")]
+        public IActionResult Update(CategoryUpdateDto updateDto)
+        {
+            Category category = new Category()
+            {
+              Id = updateDto.Id,
+              Name = updateDto.Name,
+              IsActive = updateDto.IsActive,
+              IsDeleted = updateDto.IsDeleted
+                
+               
+            };
+
+            _postgreDbContext.Update(category);
+            bool addResult = _postgreDbContext.SaveChanges() > 0;
+            if (addResult)
+                return Ok(new Result(success: true, message: Messages.CategoryUpdated));
+
+            return BadRequest(new Result(success: false, message: Messages.CategoryNotUpdated));
+        }
+
 
         [HttpGet("listwithoutjoin")]
         public IActionResult GetListWithoutJoin()
         {
-            var query = from category in dbContext.Categories
+            var query = from category in _postgreDbContext.Categories
                         where category.IsDeleted == false
                         select new CategoryDto
                         {
@@ -62,7 +85,7 @@ namespace BlogBackend2.Controllers
         [HttpGet("listwithinclude")]
         public IActionResult GetList()
         {
-            var categories = dbContext.Categories
+            var categories = _postgreDbContext.Categories
                 .Include(p => p.Articles)
                 .Where(p => p.IsDeleted == false).ToList();
 
@@ -82,6 +105,28 @@ namespace BlogBackend2.Controllers
             return Ok(new DataResult<List<CategoryDto>>(success: true, Messages.CategoryList, categoryDtos));
         }
 
+        [HttpGet("getCategoryId")]
+        public IActionResult GetCategoryId(int id)
+        {
+            var categories = _postgreDbContext.Categories
+                .Include(p => p.Articles)
+                .Where(p => p.Id == id).ToList();
+
+            List<CategoryDto> categoryDtos = new();
+
+            categories.ForEach(category =>
+            {
+                categoryDtos.Add(new CategoryDto()
+                {
+                    Id = category.Id,
+                    Name = category.Name,
+                    IsActive = category.IsActive,
+                    ArticleCount = category.Articles.Count
+                });
+            });
+
+            return Ok(new DataResult<List<CategoryDto>>(success: true, Messages.CategoryIdList, categoryDtos));
+        }
 
 
     }
